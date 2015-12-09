3b62696e9424f50cd41f7817aefa89dcb14b22ff
topleft <pete.james.jeffryes@gmail.com>
2015-12-08 20:06:37 -0700

    updated schema and seed

--- src/server/seed/initial-seed.js
+++ src/server/seed/initial-seed.js
@@ -5,6 +5,7 @@
 var server = require("../app");
 var models = require('../models/index');
 var CommonWealth;
+var RamenShop;
 var lunchMenu;
 var dinnerMenu;
 
@@ -21,7 +22,9 @@ var dinnerMenu;
             contactLastName: "Jeffryes",
             contactCellNumber: "5102891955",
             contactEmail: "pete.topleft@gmail.com",
-            occupancy: 60
+            occupancy: 60,
+            websiteUrl: "http://www.commonwealthpubs.com/visit-1-2",
+            logoUrl: "http://static1.squarespace.com/static/54e0f431e4b043f1c99a55d0/t/55b6be69e4b0e4916f65839e/1438039658902/?format=300w",
             }).then(function(space){
                 console.log("Space: ", space.dataValues);
                 CommonWealth = space.dataValues;
@@ -75,6 +78,64 @@ var dinnerMenu;
                 console.log(err);
             });
       });
+
+
+models.Space.create({
+    name: 'Ramen Shop',
+    type: 'Restaurant',
+    googlePlaceID: 'ChIJqz4ScsN9hYARA0aiLsuEmrA',
+    owner: null,
+    contactFirstName: "Pete" ,
+    contactLastName: "Jeffryes",
+    contactCellNumber: "5102891955",
+    contactEmail: "pete.topleft@gmail.com",
+    occupancy: 80,
+    websiteUrl: "http://www.ramenshop.com/",
+    logoUrl: "https://pbs.twimg.com/profile_images/2825870211/5c2e994c25fb347543f8fe5924657274_400x400.png",
+    }).then(function(space){
+        RamenShop = space.dataValues;
+        console.log('_____________________________________________________________');
+              models.Menu.create({
+                  name: "Tsukemen",
+                  bevItems: ['Beer', 'Soda', 'Water'],
+                  foodItems: ['Tsukemen ramen', 'Donburi', 'Pickles', 'Mochi'],
+                  costPerPerson: 30.00,
+                  SpaceId: RamenShop.id,
+                  imageUrl: 'http://cdn-image.foodandwine.com/sites/default/files/original-201310-HD-best-ramen-shops-ramen-shop.jpg'
+              }).then(function(menu){
+                  lunchMenu = menu.dataValues;
+                  console.log('_____________________________________________________________');
+                  console.log('\n');
+                  models.Menu.create({
+                    name: 'Tamarind Pork Ribs',
+                    bevItems: ['Beer', 'Wine', 'Sparkling Water'],
+                    foodItems: ['Tamarind Pork Ribs', 'Fried Rice', 'Chicory Salad', 'Walnut Torte'],
+                    costPerPerson: 34.00,
+                    SpaceId: RamenShop.id,
+                    imageUrl: 'http://static01.nyt.com/images/2014/05/04/fashion/05OAKLAND1/05OAKLAND1-master315.jpg'
+                }).then(function(menu){
+                    dinnerMenu = menu.dataValues;
+                      models.Event.create({
+                      SpaceId: RamenShop.id,
+                      UserId: null,
+                      date: new Date(), 
+                      time: "6:00pm", 
+                      totalGuests: 0, 
+                      cost: 0, 
+                      specialRequests: null, 
+                      inviteList: null, 
+                      barTab: 0
+                    }).catch(function(err) {
+                        console.log(err);
+                    });
+                  });
+                }).catch(function(err) {
+                    console.log(err);
+                });
+      }).catch(function(err) {
+        console.log(err);
+      });
+
 
 
 
